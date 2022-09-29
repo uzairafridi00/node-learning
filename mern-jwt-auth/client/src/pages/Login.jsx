e7db42f6c02b8360adfbf93e466fe2512265485a
Uzair Afridi <uzairafridi00@gmail.com>
2022-09-29 09:02:12 +0500

    Login Component Completed

--- mern-jwt-auth/client/src/pages/Login.jsx
+++ mern-jwt-auth/client/src/pages/Login.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
 function Login() {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <div className="container">
+        <h2>Login</h2>
+        <form action="" onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={(e) =>
+                setValues({ ...values, [e.target.name]: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={(e) =>
+                setValues({ ...values, [e.target.name]: e.target.value })
+              }
+            />
+          </div>
+          <button type="submit">Submit</button>
+          <span>
+            Don't have an account? <Link to="/register">Register</Link>
+          </span>
+        </form>
+        <ToastContainer />
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
